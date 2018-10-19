795b47b0e99cf626373e84dcb01b774d96ebfb3b
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-19 21:32:33 +0900

    add Repository interface

--- modules/example-api/domain/src/main/scala/example/exampleApi/domain/repository/user/UserRepository.scala
+++ modules/example-api/domain/src/main/scala/example/exampleApi/domain/repository/user/UserRepository.scala
@@ -1,5 +1,9 @@
 package example.exampleApi.domain.repository.user
+import example.exampleApi.domain.model.user.{ User, UserId }
+import example.shared.lib.dddSupport.adapter.secondary.repository.Repository
 
-class UserRepository {
-
+abstract class UserRepository extends Repository[UserId, User] {
+  def resolveById(id: UserId): Option[User]
+  def store(entity: User): User
+  def remove(id: UserId): Unit
 }
