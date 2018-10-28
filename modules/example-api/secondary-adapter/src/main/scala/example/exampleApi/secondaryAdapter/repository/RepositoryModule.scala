c92ad840f6f59734e864ce4434c4ad4d06edf29d
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 05:57:06 +0900

    add RepositoryModule

---
+++ modules/example-api/secondary-adapter/src/main/scala/example/exampleApi/secondaryAdapter/repository/RepositoryModule.scala
@@ -0,0 +1,14 @@
+package example.exampleApi.secondaryAdapter.repository
+
+import com.google.inject.AbstractModule
+
+import example.exampleApi.domain.repository.user.UserRepository
+import example.exampleApi.secondaryAdapter.repository.user.UserRepositoryImpl
+
+class RepositoryModule extends AbstractModule {
+
+  def configure(): Unit = {
+    bind(classOf[UserRepository]).to(classOf[UserRepositoryImpl])
+  }
+
+}
