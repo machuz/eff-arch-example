a3b60bf025d3ea83646233c8ce0d0161b7942c99
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 05:49:46 +0900

    fix InterpreterModule

--- modules/shared/secondary-adapter/src/main/scala/example/shared/adapter/config/di/InterpreterModule.scala
+++ modules/shared/secondary-adapter/src/main/scala/example/shared/adapter/config/di/InterpreterModule.scala
@@ -3,19 +3,15 @@ package example.shared.adapter.config.di
 import com.google.inject.AbstractModule
 
 import example.shared.lib.dddSupport.domain.{ UUIDIdGenerator, UUIDIdGeneratorImpl }
-import example.shared.lib.eff.util.clock.joda.{ JodaTimeUtils, JodaTimeUtilsImpl }
-import example.shared.lib.eff.util.clock.joda.interpreter.{ JodaTimeMInterpreter, JodaTimeMInterpreterImpl }
+import example.shared.lib.eff.util.clock.java8.interpreter.{ ClockMInterpreter, ClockMInterpreterImpl }
 import example.shared.lib.eff.util.idGen.interpreter.{ IdGenInterpreter, IdGenInterpreterImpl }
 
 class InterpreterModule extends AbstractModule {
 
   def configure(): Unit = {
-//    bind(classOf[DBComponent]).toProvider(classOf[DBProvider])
-//    bind(classOf[DBIOInterpreter]).to(classOf[DBIOInterpreterImpl])
-    bind(classOf[JodaTimeUtils]).to(classOf[JodaTimeUtilsImpl])
     bind(classOf[UUIDIdGenerator]).to(classOf[UUIDIdGeneratorImpl])
     bind(classOf[IdGenInterpreter]).to(classOf[IdGenInterpreterImpl])
-    bind(classOf[JodaTimeMInterpreter]).to(classOf[JodaTimeMInterpreterImpl])
+    bind(classOf[ClockMInterpreter]).to(classOf[ClockMInterpreterImpl])
   }
 
 }
