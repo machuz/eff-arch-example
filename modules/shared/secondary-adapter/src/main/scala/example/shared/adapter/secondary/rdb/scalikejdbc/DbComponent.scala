b012105b52d83a15388488f06c85e51f9f238239
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 04:51:10 +0900

    add DdComponent

---
+++ modules/shared/secondary-adapter/src/main/scala/example/shared/adapter/secondary/rdb/scalikejdbc/DbComponent.scala
@@ -0,0 +1,30 @@
+package example.shared.adapter.secondary.rdb.scalikejdbc
+
+import com.zaxxer.hikari.{ HikariConfig, HikariDataSource }
+
+import javax.inject.{ Inject, Singleton }
+import scalikejdbc._
+
+@Singleton
+class DbComponent @Inject()(
+  dbConfig: ScalikejdbcConfig
+) {
+
+  def initialize(): Unit = {
+    val dataSource: HikariDataSource = {
+      val hc = new HikariConfig()
+      hc.setDriverClassName(dbConfig.rdb.driver)
+      hc.setJdbcUrl(dbConfig.rdb.url)
+      hc.setUsername(dbConfig.rdb.user)
+      hc.setPassword(dbConfig.rdb.password)
+      hc.setPoolName(dbConfig.rdb.poolName)
+      hc.setMinimumIdle(dbConfig.rdb.poolInitialSize)
+      hc.setMaximumPoolSize(dbConfig.rdb.poolMaxSize)
+      hc.setConnectionTimeout(dbConfig.rdb.poolConnectionTimeoutMillis)
+      hc.setConnectionTestQuery(dbConfig.rdb.poolValidationQuery)
+      new HikariDataSource(hc)
+    }
+    ConnectionPool.singleton(new DataSourceConnectionPool(dataSource))
+  }
+
+}
