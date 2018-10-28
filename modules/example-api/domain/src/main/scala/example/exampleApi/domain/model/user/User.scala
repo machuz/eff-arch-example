aa1fbae9e0a6be24a040d4150ce68ea23e6de972
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 05:47:10 +0900

    add applyEff

--- modules/example-api/domain/src/main/scala/example/exampleApi/domain/model/user/User.scala
+++ modules/example-api/domain/src/main/scala/example/exampleApi/domain/model/user/User.scala
@@ -1,13 +1,37 @@
 package example.exampleApi.domain.model.user
+import org.atnos.eff.Eff
+
 import example.shared.lib.dddSupport.domain.Entity
 
 import java.time.ZonedDateTime
+import example.shared.lib.eff._
+import example.shared.lib.eff.util.clock.java8.ClockM
+import example.shared.lib.eff.util.idGen.IdGen
 
-case class User(
+case class User private (
   id: UserId,
   name: Option[String],
   createdAt: ZonedDateTime,
   updatedAt: ZonedDateTime
 ) extends Entity[UserId] {
   override val identifier: UserId = id
+}
+
+object User {
+
+  def applyEff[R: _idgen: _clockm](
+    name: Option[String]
+  ): Eff[R, User] = {
+    for {
+      id  <- UserId.generate[R]
+      now <- ClockM.zonedNow[R]()
+    } yield
+      User(
+        id = id,
+        name = name,
+        createdAt = now,
+        updatedAt = now
+      )
+  }
+
 }
