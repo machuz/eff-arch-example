0f7c3b4d96e4b5f1f951c8629a3c77142c51252c
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 02:29:45 +0900

    impl UserRepository

--- modules/example-api/domain/src/main/scala/example/exampleApi/domain/repository/user/UserRepository.scala
+++ modules/example-api/domain/src/main/scala/example/exampleApi/domain/repository/user/UserRepository.scala
@@ -1,9 +1,12 @@
 package example.exampleApi.domain.repository.user
+import org.atnos.eff.Eff
+
 import example.exampleApi.domain.model.user.{ User, UserId }
 import example.shared.lib.dddSupport.adapter.secondary.repository.Repository
+import example.shared.lib.eff._trantask
 
 abstract class UserRepository extends Repository[UserId, User] {
-  def resolveById(id: UserId): Option[User]
-  def store(entity: User): User
-  def remove(id: UserId): Unit
+  def resolveById[R: _trantask](id: UserId): Eff[R, Option[User]]
+  def store[R: _trantask](entity: User): Eff[R, User]
+  def remove[R: _trantask](id: UserId): Eff[R, Unit]
 }
---
+++ modules/example-api/secondary-adapter/src/main/scala/example/exampleApi/secondaryAdapter/repository/user/UserRepositoryImpl.scala
@@ -0,0 +1,66 @@
+package example.exampleApi.secondaryAdapter.repository.user
+
+import org.atnos.eff.Eff
+
+import example.exampleApi.domain.model.user.{ User, UserId }
+import example.exampleApi.domain.repository.user.UserRepository
+import example.exampleApi.secondaryAdapter.db.dataModel.UserDataModel
+import example.shared.adapter.secondary.transactionTask.scalikejdbc._
+import example.shared.adapter.secondary.rdb.scalikejdbc.pimp.RichMySQLSyntaxSupport._
+import example.shared.lib.eff._
+import scalikejdbc._
+
+class UserRepositoryImpl extends UserRepository with UserConverter {
+
+  private val u = UserDataModel.syntax("u")
+
+  override def resolveById[R: _trantask](id: UserId): Eff[R, Option[User]] = {
+    val res = ask.map { implicit session =>
+      withSQL {
+        select
+          .from(UserDataModel as u)
+          .where
+          .eq(u.id, id.value)
+      }.map(UserDataModel(u.resultName))
+        .single
+        .apply
+        .map(convertToDomainModel)
+    }
+    fromTranTask(res)
+  }
+
+  override def store[R: _trantask](entity: User): Eff[R, User] = {
+    val res = ask.map { implicit session =>
+      withSQL {
+        insert
+          .into(UserDataModel)
+          .namedValues(
+            u.id        -> entity.id.value,
+            u.name      -> entity.name,
+            u.createdAt -> entity.createdAt,
+            u.updatedAt -> entity.updatedAt
+          )
+          .onDuplicateKeyUpdate(
+            u.id -> entity.id.value
+          )
+      }.update().apply()
+      entity
+    }
+    fromTranTask(res)
+  }
+
+  override def remove[R: _trantask](id: UserId): Eff[R, Unit] = {
+    val res = ask
+      .map { implicit session =>
+        withSQL {
+          delete
+            .from(UserDataModel)
+            .where
+            .eq(u.id, id.value)
+        }.update.apply()
+      }
+      .map(_ => ())
+    fromTranTask(res)
+  }
+
+}
