6294cd7527f85a7416dc19603594be0453b6ac95
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-10-29 05:55:10 +0900

    add UseCaseModule

---
+++ modules/example-api/usecase/src/main/scala/example/exampleApi/usecase/UseCaseModule.scala
@@ -0,0 +1,15 @@
+package example.exampleApi.usecase
+
+import com.google.inject.AbstractModule
+
+import example.exampleApi.usecase.user.create.{ CreateUserUseCase, CreateUserUseCaseImpl }
+import example.exampleApi.usecase.user.show.{ ShowUserUseCase, ShowUserUseCaseImpl }
+
+class UseCaseModule extends AbstractModule {
+
+  def configure(): Unit = {
+    bind(classOf[ShowUserUseCase]).to(classOf[ShowUserUseCaseImpl])
+    bind(classOf[CreateUserUseCase]).to(classOf[CreateUserUseCaseImpl])
+  }
+
+}
