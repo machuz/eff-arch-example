4acf497db61c8f050b0b1a43f66c32a8444156de
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-11-06 07:32:28 +0900

    clean

--- modules/example-api/external-adapter/src/main/scala/example/ExampleApiServer.scala
+++ modules/example-api/external-adapter/src/main/scala/example/ExampleApiServer.scala
@@ -5,9 +5,13 @@ import com.google.inject.Inject
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{ ContentTypes, HttpEntity }
 import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
 import example.akkaHttp.AbstractAkkaHttpServer
 import example.config.AkkaHttpServerConf
 import example.config.di.Injector
+import javax.inject.Named
+
+import scala.concurrent.ExecutionContext
 
 object ExampleApiServer extends App with Injector {
   val server = injector.getInstance(classOf[ExampleApiServer])
@@ -15,9 +19,13 @@ object ExampleApiServer extends App with Injector {
 }
 
 class ExampleApiServer @Inject()(
-  akkaHttpServerConf: AkkaHttpServerConf
-)(implicit actorSystem: ActorSystem)
-  extends AbstractAkkaHttpServer(akkaHttpServerConf) {
+  override val akkaHttpServerConf: AkkaHttpServerConf
+)(
+  implicit override val actorSystem: ActorSystem,
+  override val actorMaterializer: ActorMaterializer,
+  @Named("default-app-context") override val ec: ExecutionContext
+) extends AbstractAkkaHttpServer {
+
   override protected def routes: Route =
     path("hello") {
       get {
