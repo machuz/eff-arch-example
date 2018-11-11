4d82394886c7e396f462aa6f61879ae747ed1abe
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-11-11 20:56:35 +0900

    impl presenter

--- modules/example-api/external-adapter/src/main/scala/example/user/ShowUserPresenter.scala
+++ modules/example-api/external-adapter/src/main/scala/example/user/ShowUserPresenter.scala
@@ -1,10 +1,35 @@
 package example.user
 
-import example.akkaHttp.AbstractAkkaHttpPresenter
-import example.exampleApi.domain.model.user.User
-import example.shared.lib.dddSupport.Error
-import scalaz.\/
+import akka.http.scaladsl.model.{  HttpResponse, ResponseEntity, StatusCodes }
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.StandardRoute
+import example.akkaHttp.{ AbstractAkkaHttpPresenter, ErrorResponse }
+import example.exampleApi.usecase.user.show.ShowUserUseCaseResult
+import example.shared.lib.dddSupport.{ Error, ErrorCode }
 
-class ShowUserPresenter extends AbstractAkkaHttpPresenter[\/[Error, User]] {
-  override def response(arg: Arg): Rendered = ???
+import scalaz.{ -\/, \/, \/- }
+
+class ShowUserPresenter extends AbstractAkkaHttpPresenter[\/[Error, ShowUserUseCaseResult]] {
+  override def response(arg: \/[Error, ShowUserUseCaseResult]): StandardRoute = {
+    arg match {
+      case \/-(useCaseRes) =>
+        val httpRes = HttpResponse(
+          status = StatusCodes.OK,
+          entity = ResponseEntity(UserJsonModel.convertToJsonModel(useCaseRes.user).toJson)
+        )
+        complete(httpRes)
+      case -\/(e: Error.UseCaseError) if e.code == ErrorCode.RESOURCE_NOT_FOUND =>
+        val httpRes = HttpResponse(
+          status = StatusCodes.NotFound,
+          entity = ResponseEntity(ErrorResponse(e.code, e.getMessage).toJson)
+        )
+        complete(httpRes)
+      case -\/(e) =>
+        val httpRes = HttpResponse(
+          status = StatusCodes.InternalServerError,
+          entity = ResponseEntity(ErrorResponse(ErrorCode.SERVER_ERROR, e.getMessage).toJson)
+        )
+        complete(httpRes)
+    }
+  }
 }
