150e6a188b437eb9b758c84b5a8a4854f720e945
ma2k8 <t_matsukawa@r.recruit.co.jp>
2018-11-11 20:20:01 +0900

    fix errorCode

--- modules/shared/lib/src/main/scala/example/shared/lib/dddSupport/ErrorCode.scala
+++ modules/shared/lib/src/main/scala/example/shared/lib/dddSupport/ErrorCode.scala
@@ -3,21 +3,26 @@ package example.shared.lib.dddSupport
 /**
   * エラーコード
   */
-sealed class ErrorCode(value: Int)
+sealed class ErrorCode(value: String)
 
 object ErrorCode {
-  case object INVALID_REQUEST     extends ErrorCode(400000)
-  case object INVALID_PARAMETERS  extends ErrorCode(400100)
-  case object UNAUTHORIZED        extends ErrorCode(401000)
-  case object ACCESS_FORBIDDEN    extends ErrorCode(403000)
-  case object RESOURCE_NOT_FOUND  extends ErrorCode(404000)
-  case object CONFLICT            extends ErrorCode(409000)
-  case object SERVER_ERROR        extends ErrorCode(500000)
-  case object EXTERNAL_API_ERROR  extends ErrorCode(500104)
-  case object JSON_PARSE_ERROR    extends ErrorCode(500105)
-  case object HTTP_REQUEST_ERROR  extends ErrorCode(500106)
-  case object REDIS_REQUEST_ERROR extends ErrorCode(500107)
-  case object REDIS_COMMAND_ERROR extends ErrorCode(500108)
-  case object AWS_API_ERROR       extends ErrorCode(500109)
+  case object INVALID_REQUEST     extends ErrorCode("INVALID_REQUEST")
+
+  case object INVALID_PARAMETERS  extends ErrorCode("INVALID_PARAMETERS")
+
+  case object UNAUTHORIZED        extends ErrorCode("UNAUTHORIZED")
+
+  case object ACCESS_FORBIDDEN    extends ErrorCode("ACCESS_FORBIDDEN")
+
+  case object RESOURCE_NOT_FOUND  extends ErrorCode("RESOURCE_NOT_FOUND")
+
+  case object CONFLICT            extends ErrorCode("CONFLICT")
+
+  case object SERVER_ERROR        extends ErrorCode("SERVER_ERROR")
+  case object JSON_PARSE_ERROR    extends ErrorCode("JSON_PARSE_ERROR")
+  case object HTTP_REQUEST_ERROR  extends ErrorCode("HTTP_REQUEST_ERROR")
+  case object REDIS_REQUEST_ERROR extends ErrorCode("REDIS_REQUEST_ERROR")
+  case object REDIS_COMMAND_ERROR extends ErrorCode("REDIS_COMMAND_ERROR")
+  case object AWS_API_ERROR       extends ErrorCode("AWS_API_ERROR")
 
 }
